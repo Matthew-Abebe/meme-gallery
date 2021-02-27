487b0cae9ac986e3e41e27bc9cedda39cb2ef641
Matthew Abebe <matthewabebe.inbox@gmail.com>
2021-02-27 14:15:41 -0800

    Completed Meme Gallery project

---
+++ main.js
@@ -0,0 +1,26 @@
+var imageUrls = ['https://i.kym-cdn.com/entries/icons/original/000/013/564/doge.jpg',
+  'https://etcanada.com/wp-content/uploads/2016/06/58c0500b2ac4c342c6fc4ab096ada150.jpg?quality=80&strip=all&crop=0px%2C21px%2C500px%2C333px&resize=720%2C480',
+  'https://wiki.godvillegame.com/images/thumb/7/76/Prap.jpg/298px-Prap.jpg','https://i.pinimg.com/474x/a6/59/b5/a659b55ccf0bb7fcceeb901226768848.jpg','https://64.media.tumblr.com/30243eb75aa86ee15c7f7f40923b148e/tumblr_pak3z2ET3g1r9qwkso1_500.jpg'];
+var gallery = document.querySelector('main');
+var imageUrlInput = document.querySelector('input');
+var addImageButton = document.querySelector('button');
+
+addImageButton.addEventListener('click', function () {
+ if (imageUrlInput.value !== '') {
+  imageUrls.push(imageUrlInput.value);
+ }
+  imageUrlInput.value = '';
+  updateGallery();
+});
+
+function updateGallery() {
+  gallery.innerHTML = '';
+  for (var i = 0; i < imageUrls.length; i++) {
+    var imageElement = document.createElement('img');
+    imageElement.className = 'gallery-image';
+    imageElement.src = imageUrls[i];
+    gallery.appendChild(imageElement);
+  }
+}
+
+updateGallery();
